c484546fa7d46aa2ebdc83c576020927ee29cfc3
gesielrosa <gesielr@gmail.com>
2023-07-30 18:24:04 -0300

    feat(app): add members routing to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'members',
+    pathMatch: 'full',
+  },
+  {
+    path: 'members',
+    loadChildren: () => import('./features/members/pages/members-routing.module').then((m) => m.MembersRoutingModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
