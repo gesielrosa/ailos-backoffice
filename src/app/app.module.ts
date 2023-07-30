df5c50ac0fcc156750a482a85856079a194c7343
gesielrosa <gesielr@gmail.com>
2023-07-30 18:24:40 -0300

    feat(app): add layout and nav components to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {provideNgxMask} from 'ngx-mask';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LayoutComponent} from './ui/layout/layout.component';
+import {HeaderComponent} from './ui/header/header.component';
+import {NavComponent} from './ui/nav/nav.component';
+import {NavItemComponent} from './ui/nav/nav-item/nav-item.component';
+import {FakeMemberRequestInterceptor} from './shared/interceptors/fake-member-request.interceptor';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    LayoutComponent,
+    HeaderComponent,
+    NavComponent,
+    NavItemComponent,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    provideNgxMask(),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: FakeMemberRequestInterceptor,
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
